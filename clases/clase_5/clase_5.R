48913340f7d3111c88f165c46892e8960e3f129c
andresrabinovich <andresrabinovich@gmail.com>
2022-11-08 13:09:29 -0300

    Agrega clase 5

--- clases/clase_5/clase_5.R
+++ clases/clase_5/clase_5.R
@@ -8,7 +8,7 @@
 #de nuevas observaciones.
 
 #Veamos un dataset de medidas de peces. Ancho y alto (?) o largo
-fish <- read.csv("~/cursos/analisis_de_datos_con_r_diciembre_2021/clases/clase_5/fish.csv")
+fish <- read.csv("~/cursos/analisis_de_datos_con_r_noviembre_2022/clases/clase_5/fish.csv")
 plot(fish$height, fish$width)
 
 #Qué podemos decir de estos peces viendo sus medidas? Tendran algo en comun? Hay grupos de peces?
@@ -24,7 +24,7 @@ plot(fish_escaleado$height, fish_escaleado$width, xlim = c(-2, 2), ylim = c(-2, 
 
 #Este dataset era facil, un poco de juguete, veamos alguno mas real, con mas dimensiones
 
-mamiferos <- read.csv("~/cursos/analisis_de_datos_con_r_diciembre_2021/clases/clase_5/mamiferos.csv")
+mamiferos <- read.csv("~/cursos/analisis_de_datos_con_r_noviembre_2022/clases/clase_5/mamiferos.csv")
 View(mamiferos)
 nombres <- mamiferos$name #Me guardo los nombres
 mamiferos <- mamiferos[, -1] #Me quedo solo con los datos
@@ -117,7 +117,7 @@ plot(2:6, sse, type = "b", xlab = "k") #Criterio del codo
 #Otra forma de ver esto es usando un grafico de siluetas 
 #-------------------------Este codigo no importa!
 require(jpeg)
-img<-readJPEG("~/cursos/analisis_de_datos_con_r_diciembre_2021/clases/clase_5/sil.jpg")
+img<-readJPEG("~/cursos/analisis_de_datos_con_r_noviembre_2022/clases/clase_5/sil.jpg")
 plot(1:10,ty="n", xlim = c(0, 10), ylim = c(0, 10))
 rasterImage(img,0,0,10,10)
 #-------------------------
@@ -132,7 +132,7 @@ plot(silhouette(clusters$cluster, dist(fish_escaleado)))
 
 #Veamos otro dataset. Pokemon
 #write.csv(Pokemon[, c("Name", "HP", "Attack", "Defense", "Sp..Atk", "Sp..Def", "Speed")], file="pokemon.csv", row.names = F)
-pokemon <- read.csv("~/cursos/analisis_de_datos_con_r_diciembre_2021/clases/clase_5/pokemon.csv")
+pokemon <- read.csv("~/cursos/analisis_de_datos_con_r_noviembre_2022/clases/clase_5/pokemon.csv")
 View(pokemon)
 #Nos quedamos con los nombres y los sacamos del dataset
 nombres <- pokemon$Name
@@ -203,3 +203,4 @@ nombres_normales[clusters_normales == 1]
 
 #Existen otros algoritmos de clusterizacion que miden otras cosas, como por ejemplo, dbscan. En general, cuanto mas complejos
 #los algoritmos, mas parametros tienen y mas decisiones tenemos que tomar. Exploren!
+
